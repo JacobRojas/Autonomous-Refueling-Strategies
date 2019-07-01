01807b689851ae3d42d242b89fc7c4cca81cbbe8
kamalca <sonca1999@outlook.com>
2019-07-01 14:37:58 -0500

    Update scrape.py API
    I can make a good API, I promise. haha
    
    usage:
    python scrape.py [filename(s)]

--- scrape.py
+++ scrape.py
@@ -1,28 +1,22 @@
 import sys
 from bs4 import BeautifulSoup
 
-source = 'Trip1.html'
-if(len(sys.argv) == 2):
-	source = 'Trip' + sys.argv[1] + '.html'
-else:
-	print('usage: ' + sys.argv[0] + ' [Trip num]')
-	quit()
+for i in range(1, len(sys.argv)):
+	raw = open(sys.argv[i]).read()
+	html = BeautifulSoup(raw, 'html.parser')
+	file = open('Trip' + str(i) + '.csv', 'w')
 
-raw = open(source).read()
-html = BeautifulSoup(raw, 'html.parser')
-file = open('trip.csv', 'w')
+	html = html.find(id="steps")
 
-html = html.find(id="steps")
-
-for listItem in html.select('div'):
-	if 'row' in listItem['class']:
-		if listItem.a == None:
-			for d in listItem.div.div.select('div'):
-				if 'stationInfo' in d['class']:
-					#print(d.b.text)
-					file.write("1,0\n")
-		else:
-			for d in listItem.a.div.div.select('div'):
-				if 'fuelPrice' in d['class']:
-					#print([x.strip() for x in d.text.split('\n') if x][0])
-					file.write('1,' + [x.strip() for x in d.text.split('\n') if x][0] + '\n')
+	for listItem in html.select('div'):
+		if 'row' in listItem['class']:
+			if listItem.a == None:
+				for d in listItem.div.div.select('div'):
+					if 'stationInfo' in d['class']:
+						#print(d.b.text)
+						file.write("1,0\n")
+			else:
+				for d in listItem.a.div.div.select('div'):
+					if 'fuelPrice' in d['class']:
+						#print([x.strip() for x in d.text.split('\n') if x][0])
+						file.write('1,' + [x.strip() for x in d.text.split('\n') if x][0] + '\n')
