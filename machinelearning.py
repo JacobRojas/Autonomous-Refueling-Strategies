99c1ea865a89e87e482c020d78e87b58da8acd7d
kamalca <sonca1999@outlook.com>
2019-07-23 11:49:24 -0500

    Update machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -1,5 +1,7 @@
 #First we load the necessary libraries and initialize a graph, as follows:
 import sys
+import random
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -21,17 +23,24 @@ for line in file:
 # iris = datasets.load_iris()
 # x_vals = np.array([[x[1], x[2], x[3]] for x in iris.data])
 # y_vals = np.array([y[0] for y in iris.data])
-x_vals = np.array(x)
-y_vals = np.array(y)
 
-print(x_vals)
-print(y_vals)
+split = math.floor(len(x) * 0.75)
+
+x_vals = np.array(x[:split])
+y_vals = np.array(y[:split])
+
+x_test = np.array(x[split:])
+y_test = np.array(y[split:])
+
+print("Training Set = " + str(len(x_vals)))
+print("Testing Set = " + str(len(x_test)))
 
 #Next we declare the batch size, placeholders, variables, and model output.
 #The only difference here is that we change the size specifications of the 
 #x data placeholder to take three values instead of one, as follows:
-batch_size = 25
-learning_rate = 0.0000001
+batch_size = math.floor(len(x_vals))
+test_size = len(x_test)
+learning_rate = 0.0007
 x_data = tf.placeholder(shape=[None, 3], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 A = tf.Variable(tf.random_normal(shape=[3,1]))
@@ -55,22 +64,54 @@ sess.run(init)
 my_opt = tf.train.GradientDescentOptimizer(learning_rate)
 train_step = my_opt.minimize(loss)
 loss_vec = []
-for i in range(100):
-    rand_index = np.random.choice(len(x_vals), size=batch_size)
+test_loss = []
+for i in range(1000):
+    #rand_index = np.random.choice(len(x_vals), size=batch_size)
+    index = list(range(len(x_vals)))
+    random.shuffle(index)
+    rand_index = np.array(index[:batch_size])
     rand_x = x_vals[rand_index]
     rand_y = np.transpose([y_vals[rand_index]])
-    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
-    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
-    loss_vec.append(temp_loss[0])
+    #sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+    temp_loss = sess.run([loss, train_step], feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(temp_loss[0][0])
+
+    #rand_index = np.random.choice(len(x_vals), size=batch_size)
+    index = list(range(len(x_test)))
+    random.shuffle(index)
+    rand_index = np.array(index[:test_size])
+    rand_x = x_test[rand_index]
+    rand_y = np.transpose([y_test[rand_index]])
+    #sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+    temp_test = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    test_loss.append(temp_test[0])
+
     if (i+1)%250==0:
-        print('Step #' + str(i+1) + ' A = ' + 
-        	str(sess.run(A)) + ' b = ' + str(sess.run(b)))
-        print('Loss = ' + str(temp_loss))
+        print('Step #' + str(i+1) + '\nA = ' + 
+        	str(sess.run(A)) + '\nb = ' + str(sess.run(b)))
+        print('Loss = ' + str(temp_loss[0][0]))
+        print('Test = ' + str(temp_test[0]))
+
+
+#Give estimates
+for i in range(len(x)):
+    print("Trip " + str(i+1))
+    print('  x = ' + str(x[i]))
+    print('  y = ' + str(y[i]))
+    print('  guess = ' + str(
+      sess.run(model_output, feed_dict=
+      {x_data: np.array([x[i]]), y_target: np.array([[y[i]]])})))
+
+#Save the model
+saver = tf.train.Saver()
+saver.save(sess, '/Users/kameroncarr/Documents/GitHub/TxSt2019/model.ckpt')
 
 #Now we can observe the loss over the training 
 #iterations to be sure that it converged, as follows:
 plt.plot(loss_vec, 'k-')
+plt.plot(test_loss, 'k--')
 plt.title('Loss vs. Generation')
 plt.xlabel('Generation')
 plt.ylabel('Loss')
+#plt.yscale("log")
 plt.show()
