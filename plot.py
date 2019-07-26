a9d3581468760b2226a7af3cc1e74661a05c47c2
kamalca <sonca1999@outlook.com>
2019-07-26 09:45:28 -0500

    New Model && Fix plot.py
    With new scrape model, `plot.py` needed a quick fix.
    
    New model moves `startSecretary` and `startCritical` earlier from 0.6 and 0.85 to 0.55 and 0.8.
    
    Machine learning model has lower error and will hopefully perform better with running out of gas.

--- plot.py
+++ plot.py
@@ -8,6 +8,8 @@ path = []
 file = open(sys.argv[1], 'r')
 for line in file:
 	data = [float(d.strip()) for d in line.split(',')]
+	for i in range(int(data[0]) - 1):
+		path.append(0)
 	path.append(data[1])
 	if data[1] != 0:
 		freq[data[1]] = freq.get(data[1], 0) + 1
